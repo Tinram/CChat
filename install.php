b5adcb4befea7d9b0ffb46dd37bb8c0520bfa25b
Tinram <Tinram@users.noreply.github.com>
2017-01-06 23:16:51 +0000

    revise MySQL schema and install.php

--- install.php
+++ install.php
@@ -2,11 +2,12 @@
 
 /**
 	* Very simple install script to setup the database for CChat.
-	* Define your constants in the configuration section below, then load this via your web server, or via the command-line: php -f install.php
+	* Define your constants in the configuration section below, then load this file via your web server
+	* (or via the command-line: php -f install.php).
 	*
 	* @author            Martin Latter <copysense.co.uk>
 	* @copyright         29/06/2014
-	* @version           0.03
+	* @version           0.04
 	* @license           GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html
 	* @link              https://github.com/Tinram/cchat.git
 */
@@ -19,7 +20,7 @@ define('APP_PASSWORD', 'password');
 define('APP_NAME', 'CChat');
 
 define('SUPER_USER', 'root');
-define('SUPER_USER_PASSWORD', 'root_password');
+define('SUPER_USER_PASSWORD', '**root_password**');
 
 define('HOST', 'localhost');
 define('DATABASE', 'cchat');
@@ -31,24 +32,35 @@ define('COLLATION', 'latin1_general_ci');
 /* END CONFIGURATION */
 
 
+# command-line or server line break output
+define('LINE_BREAK', (PHP_SAPI !== 'cli') ? '<br>' : "\n");
+
+
 $oConnection = new mysqli(HOST, SUPER_USER, SUPER_USER_PASSWORD);
 
 if ($oConnection->connect_errno) {
-	die('Database connection failed: ' . $oConnection->connect_errno . ') ' . $oConnection->connect_error);
+	die('Database connection failed: ' . $oConnection->connect_errno . ') ' . $oConnection->connect_error . LINE_BREAK);
 }
 else {
 
-	echo '<h2>' . APP_NAME . ' Database Setup</h2>';
+	$sTitle = APP_NAME . ' Database Setup';
+
+	if (PHP_SAPI !== 'cli') {
+		echo '<h2>' . $sTitle . '</h2>';
+	}
+	else {
+		echo $sTitle . "\n";
+	}
 
 	# create database
 	$sQuery = 'CREATE DATABASE IF NOT EXISTS ' . DATABASE . ' CHARACTER SET ' . CHARSET . ' COLLATE ' . COLLATION;
 	$rResults = $oConnection->query($sQuery);
 
 	if ($rResults) {
-		echo 'Created database ' . DATABASE . '.<br>';
+		echo 'Created database ' . DATABASE . '.' . LINE_BREAK;
 	}
 	else {
-		die('ERROR: could not create the ' . DATABASE . ' database.');
+		die('ERROR: could not create the ' . DATABASE . ' database.' . LINE_BREAK);
 	}
 
 	# select database
@@ -57,22 +69,22 @@ else {
 
 	# create table
 	$sQuery = '
-		CREATE TABLE `' . TABLE . '` (
+		CREATE TABLE IF NOT EXISTS `' . TABLE . '` (
 			`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-			`name` VARCHAR(15) NOT NULL,
+			`name` CHAR(15) NOT NULL,
 			`message` VARCHAR(384) NOT NULL,
 			`date` INT(10) UNSIGNED NOT NULL,
 			KEY `kdate` (`date`),
 			PRIMARY KEY (`id`)
-		) ENGINE=InnoDB DEFAULT CHARSET=latin1';
+		) ENGINE=InnoDB DEFAULT CHARSET=' . CHARSET;
 
 	$rResults = $oConnection->query($sQuery);
 
 	if ($rResults) {
-		echo 'Created table ' . TABLE . '.<br>';
+		echo 'Created table ' . TABLE . '.' . LINE_BREAK;
 	}
 	else {
-		die('ERROR: could not create the ' . TABLE . ' table.');
+		die('ERROR: could not create the ' . TABLE . ' table.' . LINE_BREAK);
 	}
 
 	# create first (unencrypted) test message
@@ -80,10 +92,10 @@ else {
 	$rResults = $oConnection->query($sQuery);
 
 	if ($rResults) {
-		echo 'Created first ' . APP_NAME . ' message.<br>';
+		echo 'Created first ' . APP_NAME . ' message.' . LINE_BREAK;
 	}
 	else {
-		die('ERROR: could not create the required first message.');
+		die('ERROR: could not create the required first message.' . LINE_BREAK);
 	}
 
 	# create grants to cchat user
@@ -91,10 +103,10 @@ else {
 	$rResults = $oConnection->query($sQuery);
 
 	if ($rResults) {
-		echo 'Created ' . APP_NAME . ' database user.<br>';
+		echo 'Created ' . APP_NAME . ' database user.' . LINE_BREAK;
 	}
 	else {
-		die('ERROR: could not create the required ' . APP_NAME . ' database user.');
+		die('ERROR: could not create the required ' . APP_NAME . ' database user.' . LINE_BREAK);
 	}
 
 	# flush
--- sql/cchat.sql
+++ sql/cchat.sql
@@ -2,10 +2,13 @@
 CREATE DATABASE cchat CHARACTER SET latin1 COLLATE latin1_general_ci;
 
 
+USE cchat;
+
+
 CREATE TABLE `chatbox` (
 
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(15) NOT NULL,
+	`name` CHAR(15) NOT NULL,
 	`message` VARCHAR(384) NOT NULL, -- 255 = ~352 bytes of base64
 	`date` INT(10) UNSIGNED NOT NULL,
 	KEY `kdate` (`date`),
