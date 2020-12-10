2b7a7b014c47ff96932ddcfb28f6b86228316260
Tinram <Tinram@users.noreply.github.com>
2020-12-10 13:06:55 +0000

    revise schema for MySQL 8

--- sql/cchat.sql
+++ sql/cchat.sql
@@ -5,13 +5,13 @@ CREATE DATABASE cchat CHARACTER SET latin1 COLLATE latin1_general_ci;
 USE cchat;
 
 
-CREATE TABLE `chatbox` (
-
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` CHAR(15) NOT NULL,
-	`message` VARCHAR(384) NOT NULL, -- 255 = ~352 bytes of base64
-	`date` INT(10) UNSIGNED NOT NULL,
-	KEY `kdate` (`date`),
-	PRIMARY KEY (`id`)
+CREATE TABLE `chatbox`
+(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` CHAR(16) NOT NULL,
+    `message` VARCHAR(384) NOT NULL COMMENT '255 = ~352 bytes of base64',
+    `date` INT UNSIGNED NOT NULL,
+    KEY `idx_date` (`date`),
+    PRIMARY KEY (`id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
